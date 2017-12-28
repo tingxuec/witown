4ca30ae88b84139107366920917fe6d9dfa26b3d
tingxuec <chqyddxxsh@163.com>
2017-12-28 23:09:08 +0800

    init

---
+++ src/main/java/com/netware/witown/WitownApplication.java
@@ -0,0 +1,12 @@
+package com.netware.witown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WitownApplication.class, args);
+	}
+}
